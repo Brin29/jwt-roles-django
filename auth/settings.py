e618b3fdb763d2d19892609032b927a104dd8e29
Brin29 <breinerstivenparracortes50@gmail.com>
2025-02-26 14:19:58 -0500

    solucion al problema de cors

--- auth/settings.py
+++ auth/settings.py
@@ -56,7 +56,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://192.168.1.2:8080'
+
+)
 
 ROOT_URLCONF = 'auth.urls'
 
